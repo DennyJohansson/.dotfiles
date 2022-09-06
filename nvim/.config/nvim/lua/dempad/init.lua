09436a723853cc3b40c0046ac0a77509c53a4f05
denny johansson <denny.joansson@hm.com>
2022-09-06 08:55:54 +0200

    asdasd ye

--- nvim/.config/nvim/lua/dempad/init.lua
+++ nvim/.config/nvim/lua/dempad/init.lua
@@ -2,7 +2,3 @@ require("dempad.set")
 require("dempad.packer")
 require("dempad.keymap")
 require("dempad.neogit")
-
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
--- nvim/.config/nvim/lua/dempad/set.lua
+++ nvim/.config/nvim/lua/dempad/set.lua
@@ -42,3 +42,6 @@ vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
 
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
