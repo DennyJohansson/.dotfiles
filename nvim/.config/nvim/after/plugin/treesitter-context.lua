40349023b839b3949137e7fc1779cddd0e277a2f
Denny Johansson <dennybjohansson@gmail.com>
2023-09-25 17:47:46 +0200

    dhd

--- nvim/.config/nvim/after/plugin/treesitter-context.lua
+++ nvim/.config/nvim/after/plugin/treesitter-context.lua
@@ -1,6 +1,6 @@
 function ContextSetup(show_all_context)
   require("treesitter-context").setup({
-    enable = true,   -- Enable this plugin (Can be enabled/disabled later via commands)
+    enable = false,   -- Enable this plugin (Can be enabled/disabled later via commands)
     throttle = true, -- Throttles plugin updates (may improve performance)
     max_lines = 0,   -- How many lines the window should span. Values <= 0 mean no limit.
     -- show_all_context = show_all_context,
