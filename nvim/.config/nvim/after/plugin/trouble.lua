7fd9db70bed93a89fce83700bc177ee611ebff6d
Denny Johansson <dennybjohansson@gmail.com>
2024-01-17 16:04:09 +0100

    add diagnostics

--- nvim/.config/nvim/after/plugin/trouble.lua
+++ nvim/.config/nvim/after/plugin/trouble.lua
@@ -1,18 +1,6 @@
-vim.keymap.set("n", "<leader>tt", "<cmd>TroubleToggle<cr>",
-  { silent = true, noremap = true }
-)
--- vim.keymap.set("n", "<leader>tw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
---   { silent = true, noremap = true }
--- )
--- vim.keymap.set("n", "<leader>td", "<cmd>TroubleToggle document_diagnostics<cr>",
---   { silent = true, noremap = true }
--- )
-vim.keymap.set("n", "<leader>tl", "<cmd>TroubleToggle loclist<cr>",
-  { silent = true, noremap = true }
-)
-vim.keymap.set("n", "<leader>tq", "<cmd>TroubleToggle quickfix<cr>",
-  { silent = true, noremap = true }
-)
-vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
-  { silent = true, noremap = true }
-)
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
--- nvim/.config/nvim/lua/dempad/remap.lua
+++ nvim/.config/nvim/lua/dempad/remap.lua
@@ -29,3 +29,9 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
