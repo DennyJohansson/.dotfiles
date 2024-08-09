93ddf5630da52745bbe117511618ddddba966606
Denny Johansson <dennybjohansson@gmail.com>
2024-08-09 12:45:41 +0200

    add meh + [b] and fix the [o]-[i]

--- phoenix/.phoenix.js
+++ phoenix/.phoenix.js
@@ -1,6 +1,8 @@
 // docs: https://kasper.github.io/phoenix/
 // based on https://github.com/azamuddin/phoenix/blob/master/.phoenix.js 
 // Preferences
+// debug: ❯ log stream --process Phoenix
+
 Phoenix.set({
   // daemon: true,
   openAtLogin: true,
@@ -20,7 +22,11 @@ const RIGHT = 'right';
 const CENTRE = 'centre';
 
 let appHistoryCursor = 0;
+// don't save duplicates, 
+// if the app is already in the history, remove it and add it to the front
 let appHistory = [];
+// TODO: support <leader> + b : go [b]ack to previous app
+let initBackApp = null;
 
 Key.on("+", MEH, () => {
   const window = Window.focused();
@@ -41,6 +47,12 @@ Key.on('c', CONTROL_SHIFT, () => {
   const window = Window.focused();
 
   if (window) {
+    window.setSize(
+      {
+        width: screen.width / 1.2,
+        height: screen.height,
+      }
+    );
     window.setTopLeft({
       x: screen.x + (screen.width / 2) - (window.frame().width / 2),
       y: screen.y + (screen.height / 2) - (window.frame().height / 2)
@@ -92,10 +104,38 @@ Key.on('f', CONTROL_SHIFT, () => {
 
 const launchApp = (appName) => {
   App.launch(appName).focus();
-  // Add to history, should be first
+  const appIndex = appHistory.indexOf(appName);
+
+  if (appIndex > -1) {
+    appHistory.splice(appIndex, 1);
+  }
+
   appHistory.unshift(appName);
   appHistoryCursor = 0;
 }
+
+const goBackToPreviousApp = () => {
+  const currentApp = appHistory[appHistoryCursor];
+
+  if (!initBackApp) {
+    // first time pressing the key
+    const prevApp = appHistory[appHistoryCursor + 1]
+
+    if (prevApp) {
+      appHistoryCursor += 1;
+      initBackApp = currentApp;
+
+      App.launch(prevApp).focus();
+    }
+  } else {
+    // second time pressing the key
+    App.launch(initBackApp).focus();
+    initBackApp = null;
+    appHistoryCursor -= 1;
+  }
+}
+
+Key.on('b', MEH, goBackToPreviousApp);
 
 Key.on('*', MEH, function() {
   launchApp('Kitty');
